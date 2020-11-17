66d9d385e093e1aeaca2e8fd90784c278b359b87
Alex Tchaikovsky <xalabam94@icloud.com>
2020-11-17 17:16:24 +0800

    Created task, new task, create, task, form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-# list all tasks
-get 'tasks', to: 'tasks#index'
+  # list all tasks
+  get 'tasks', to: 'tasks#index'
 
-#
+  # create new task
+  get 'tasks/new', to: 'tasks#new'
+
+  post 'tasks', to: 'tasks#create'
+
+  # view task details
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
